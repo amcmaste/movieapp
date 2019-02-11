8c052cb46abca1a51d149221decbb24daf131af0
Andrew McMaster <amcmaste@gmail.com>
2019-02-10 19:00:39 -0800

    Added basic templates

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 #Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, SubmitField
+from wtforms import StringField, FileField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 #Form defintion
@@ -15,4 +15,24 @@ class SignupForm(FlaskForm):
 	password = StringField('Password', id="password", validators=[DataRequired()])
 	verfiy = StringField('Confirm Password', id="verify", validators=[DataRequired(), 
 	EqualTo('password')])
+	submit = SubmitField('Submit', id="submit")
+	
+class ProfileForm(FlaskForm):
+	favorite = StringField('Favorite Movie', id="favorite", validators=[DataRequired()])
+	profile = FileField('Profile Picture', id="profile", validators=[DataRequired()])
+	submit = SubmitField('Submit', id="submit")
+	
+class MovieForm(FlaskForm):
+	title = StringField('Movie Title', id="title", validators=[DataRequired()])
+	cover = FileField('Cover Image', id="cover", validators=[DataRequired()])
+	submit = SubmitField('Submit', id="submit")
+
+class QuestionForm(FlaskForm):
+	title = StringField('Movie Title', id="title", validators=[DataRequired()])
+	question = TextAreaField('Question', id="question", validators=[DataRequired()])
+	submit = SubmitField('Submit', id="submit")
+	
+class AnswerForm(FlaskForm):
+	title = StringField('Movie Title', id="title", validators=[DataRequired()])
+	answer = TextAreaField('Answer', id="question", validators=[DataRequired()])
 	submit = SubmitField('Submit', id="submit")
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 #Imports
 from flask import render_template
 from app import app
-from app.forms import LoginForm, SignupForm
+from app.forms import LoginForm, SignupForm, ProfileForm, MovieForm, QuestionForm, AnswerForm
 
 #Route defintions
 @app.route('/', methods=['GET'])
@@ -17,3 +17,23 @@ def login():
 def signup():
 	signup = SignupForm()
 	return render_template('signup.html', form=signup)
+	
+@app.route('/editprofile', methods=['GET'])
+def profile():
+	profile = ProfileForm()
+	return render_template('editprofile.html', form=profile)
+	
+@app.route('/addmovie', methods=['GET'])
+def movie():
+	movie = MovieForm()
+	return render_template('addmovie.html', form=movie)
+	
+@app.route('/addquestion', methods=['GET'])
+def question():
+	question = QuestionForm()
+	return render_template('addquestion.html', form=question)
+	
+@app.route('/addanswer', methods=['GET'])
+def answer():
+	answer = AnswerForm()
+	return render_template('addanswer.html', form=answer)
