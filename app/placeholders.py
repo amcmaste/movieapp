3ddb2a0a56b3576ba7f2866bfc404df86cacd257
Andrew McMaster <amcmaste@gmail.com>
2019-02-15 10:58:49 -0800

    Initial design finished!

--- app/placeholders.py
+++ app/placeholders.py
@@ -25,7 +25,7 @@ class Movie(object):
 	level = 0
 	badge = ''
 
-class Question(object):
+class Question_One(object):
 	id = 301
 	user_id = 301
 	movie_id = 201
@@ -36,12 +36,104 @@ class Question(object):
 	badge = ''
 	top = 1
 
-class Answer(object):
+class Question_Two(object):
+	id = 302
+	user_id = 301
+	movie_id = 201
+	question_text = '''What does the title mean?'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+	
+class Question_Three(object):
+	id = 303
+	user_id = 301
+	movie_id = 201
+	question_text = '''Was the movie autobiographical?'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Question_Four(object):
+	id = 304
+	user_id = 301
+	movie_id = 201
+	question_text = '''What did the introduction mean?'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Question_Five(object):
+	id = 305
+	user_id = 301
+	movie_id = 201
+	question_text = '''What did the ending mean?'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Answer_One(object):
 	id = 401
 	user_id = 101
 	movie_id = 201
 	question_id = 301
 	answer_text = '''It is the name of his sled from childhood.'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Answer_Two(object):
+	id = 402
+	user_id = 101
+	movie_id = 201
+	question_id = 301
+	answer_text = '''It refers to the title character.'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+	
+class Answer_Three(object):
+	id = 403
+	user_id = 101
+	movie_id = 201
+	question_id = 301
+	answer_text = '''Possibly, many people believe that Orson Wells wrote the movie about himself.'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Answer_Four(object):
+	id = 404
+	user_id = 101
+	movie_id = 201
+	question_id = 301
+	answer_text = '''It is a metaphor for the gold standard.'''
+	create_datetime = '02-11-2019-17-00-00'
+	points = 0
+	level = 0
+	badge = ''
+	top = 1
+
+class Answer_Five(object):
+	id = 405
+	user_id = 101
+	movie_id = 201
+	question_id = 301
+	answer_text = '''It implies that we should be hopeful about the future.'''
 	create_datetime = '02-11-2019-17-00-00'
 	points = 0
 	level = 0
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm, SignupForm, ProfileForm, MovieForm, QuestionForm, AnswerForm
-from app.placeholders import User, Movie, Question, Answer
+from app.placeholders import User, Movie, Question_One, Question_Two, Question_Three, Question_Four, Question_Five, Answer_One, Answer_Two, Answer_Three, Answer_Four, Answer_Five
 
 #Route defintions
 @app.route('/', methods=['GET'])
@@ -43,6 +43,20 @@ def answer():
 def main():
 	user = User
 	movie = Movie
-	question = Question
-	answer = Answer
-	return render_template('main.html', user=user, movie=movie, question=question, answer=answer)
+	questions = [Question_One, Question_Two, Question_Three, Question_Four, Question_Five]
+	question_one = questions[0]
+	question_two = questions[1]
+	question_three = questions[2]
+	question_four = questions[3]
+	question_five = questions[4]
+	answers = [Answer_One, Answer_Two, Answer_Three, Answer_Four, Answer_Five]
+	answer_one = answers[0]
+	answer_two = answers[1]
+	answer_three = answers[2]
+	answer_four = answers[3]
+	answer_five = answers[4]
+	return render_template('main.html', user=user, movie=movie, questions=questions, 
+	question_one=question_one, question_two=question_two, question_three=question_three,
+	question_four=question_four, question_five=question_five, answers=answers, 
+	answer_one=answer_one, answer_two=answer_two, answer_three=answer_three, 
+	answer_four=answer_four, answer_five=answer_five)
